6fb7d0429553fc63dc997567ebf62aad66f085db
Costanza <60357882+costikid@users.noreply.github.com>
2023-08-16 22:11:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const slider = document.querySelector('.slider');
+      const slides = document.querySelectorAll('.slide');
+      let currentIndex = 0;
+    
+      function goToSlide(index) {
+        if (index < 0) {
+          index = slides.length - 1;
+        } else if (index >= slides.length) {
+          index = 0;
+        }
+        slider.style.transform = `translateX(-${index * 100}%)`;
+        currentIndex = index;
+      }
+    
+      function nextSlide() {
+        goToSlide(currentIndex + 1);
+      }
+    
+      function prevSlide() {
+        goToSlide(currentIndex - 1);
+      }
+    
+      // Automatically change slide every 5 seconds
+      setInterval(nextSlide, 5000);
 
+      //Add basic styles for active tabs
+$('.accordion__item-label').on('click', function() {
+    $(this).addClass('blue');
+    $(this).parent('.accordion__item').siblings().find('.accordion__item-label').removeClass('blue');
+  });
+
+  
